417703b095f2108889fc98e5d50f21cd77e52f8e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-04 13:58:33 +0100

    Tweak flags

--- bin.js
+++ bin.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-const path = require('path')
 const { pathToFileURL } = require('url')
 const { command, flag, arg, summary } = require('paparam')
+const { resolve } = require('bare-module-traverse')
 const pkg = require('./package')
 const fs = require('./fs')
 const pack = require('.')
@@ -9,19 +9,25 @@ const pack = require('.')
 const cmd = command(
   'pack',
   summary(pkg.description),
-  flag('--resolver <module>', 'The module resolver to use'),
-  flag('--base <path>', 'The base path of the module graph'),
+  flag('--platform|-p <name>', 'The operating system platform to bundle for'),
+  flag('--arch|-a <name>', 'The operating system architecture to bundle for'),
+  flag('--simulator', 'Bundle for a simulator'),
   flag('--out|-o <path>', 'The output path of the bundle'),
   arg('<entry>', 'The entry point of the module graph'),
   async (cmd) => {
-    const { resolver = 'bare-module-traverse/resolve/bare', base = '.', out } = cmd.flags
     const { entry } = cmd.args
+    const { out, platform, arch, simulator } = cmd.flags
 
-    const resolve = require(require.resolve(resolver, { paths: [path.resolve('.'), __dirname] }))
+    let bundle = await pack(pathToFileURL(entry), {
+      platform,
+      arch,
+      simulator,
+      resolve: resolve.bare
+    }, fs.readModule, fs.listPrefix)
 
-    const bundle = await pack(pathToFileURL(entry), { resolve }, fs.readModule, fs.listPrefix)
+    bundle = bundle.unmount(pathToFileURL('.'))
 
-    const buffer = bundle.unmount(pathToFileURL(base)).toBuffer()
+    const buffer = bundle.toBuffer()
 
     if (out) {
       await fs.writeFile(pathToFileURL(out), buffer)
