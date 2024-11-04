041b2f063214802ff15469541fe0644600d3177a
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-04 14:03:33 +0100

    Add `--builtins` flag

--- README.md
+++ README.md
@@ -6,6 +6,16 @@ Bundle packing for Bare.
 npm i bare-pack
 ```
 
+## Usage
+
+```js
+const pack = await
+```
+
+## API
+
+## CLI
+
 ## License
 
 Apache-2.0
--- bin.js
+++ bin.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+const path = require('path')
 const { pathToFileURL } = require('url')
 const { command, flag, arg, summary } = require('paparam')
 const { resolve } = require('bare-module-traverse')
@@ -9,20 +10,22 @@ const pack = require('.')
 const cmd = command(
   'pack',
   summary(pkg.description),
+  flag('--out|-o <path>', 'The output path of the bundle'),
+  flag('--builtins <path>', 'A list of builtin modules'),
   flag('--platform|-p <name>', 'The operating system platform to bundle for'),
   flag('--arch|-a <name>', 'The operating system architecture to bundle for'),
   flag('--simulator', 'Bundle for a simulator'),
-  flag('--out|-o <path>', 'The output path of the bundle'),
   arg('<entry>', 'The entry point of the module graph'),
   async (cmd) => {
     const { entry } = cmd.args
-    const { out, platform, arch, simulator } = cmd.flags
+    const { out, builtins, platform, arch, simulator } = cmd.flags
 
     let bundle = await pack(pathToFileURL(entry), {
       platform,
       arch,
       simulator,
-      resolve: resolve.bare
+      resolve: resolve.bare,
+      builtins: builtins ? require(path.resolve(builtins)) : []
     }, fs.readModule, fs.listPrefix)
 
     bundle = bundle.unmount(pathToFileURL('.'))
