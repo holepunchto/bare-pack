da5b2cbe510853f1824c629c494102ada0d1667b
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-04 20:35:01 +0100

    Add `--linked` flag

--- bin.js
+++ bin.js
@@ -13,19 +13,21 @@ const cmd = command(
   arg('<entry>', 'The entry point of the module graph'),
   flag('--out|-o <path>', 'The output path of the bundle'),
   flag('--builtins <path>', 'A list of builtin modules'),
+  flag('--linked', 'Resolve linked: addons instead of file: prebuilds'),
   flag('--platform|-p <name>', 'The operating system platform to bundle for'),
   flag('--arch|-a <name>', 'The operating system architecture to bundle for'),
   flag('--simulator', 'Bundle for a simulator'),
   async (cmd) => {
     const { entry } = cmd.args
-    const { out, builtins, platform, arch, simulator } = cmd.flags
+    const { out, builtins, linked, platform, arch, simulator } = cmd.flags
 
     let bundle = await pack(pathToFileURL(entry), {
       platform,
       arch,
       simulator,
       resolve: resolve.bare,
-      builtins: builtins ? require(path.resolve(builtins)) : []
+      builtins: builtins ? require(path.resolve(builtins)) : [],
+      linked
     }, fs.readModule, fs.listPrefix)
 
     bundle = bundle.unmount(pathToFileURL('.'))
